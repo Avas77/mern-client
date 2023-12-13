fa47bf0628dc0263ddc9b55754288f091d6dff23
Avas <avas.bajracharya@renegadeinsurance.com>
2023-12-13 17:27:19 +0545

    feat: added login and logo of the project

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,14 +4,13 @@ import { useEffect, useRef } from "react";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
-import { ThemedTitleV2 } from "@refinedev/mui";
-
+import { yariga } from "../assets";
 import { CredentialResponse } from "../interfaces/google";
+import { Stack } from "@mui/material";
 
 // Todo: Update your Google Client ID here
-const GOOGLE_CLIENT_ID =
-  "1041339102270-e1fpe2b6v6u1didfndh7jkjmpcashs4f.apps.googleusercontent.com";
-
+const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+console.log({ GOOGLE_CLIENT_ID });
 export const Login: React.FC = () => {
   const { mutate: login } = useLogin<CredentialResponse>();
 
@@ -61,13 +60,10 @@ export const Login: React.FC = () => {
         justifyContent="center"
         flexDirection="column"
       >
-        <ThemedTitleV2
-          collapsed={false}
-          wrapperStyles={{
-            fontSize: "22px",
-            justifyContent: "center",
-          }}
-        />
+        <Stack direction="row">
+          <img src={yariga} alt="Yariga logo" width={125} />
+          <Typography>Yariga</Typography>
+        </Stack>
 
         <GoogleButton />
 
